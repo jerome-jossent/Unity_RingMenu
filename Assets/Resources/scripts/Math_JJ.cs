ea64e312953127749781d14df19ce008660635a3
Jerome Jossent <67312941+jerome-jossent@users.noreply.github.com>
2021-03-06 23:04:24 +0100

    Calcul de Marge par translation des bords (pas rotation)

---
+++ Assets/Resources/scripts/Math_JJ.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Math_JJ
+{
+
+    public class Cercle
+    {
+        public Vector2 O;
+        public float r;
+    }
+
+    public class Droite
+    {
+        public float a;
+        public float b;
+    }
+
+    public class Intersections2D
+    {
+        public Vector2 intersection_1;
+        public Vector2 intersection_2;
+    }
+
+    public class Intersections3D
+    {
+        public Vector3 intersection_1;
+        public Vector3 intersection_2;
+    }
+
+
+    //Equation d'un cercle  (1) : (x - Ox)² + (y - Oy)² = c.r ²
+    //Equation d'une droite (2) : y = a * x + b
+    //(2) dans (1) : (x - Ox)² +            (a * x + b - Oy)² = r²
+    //g = (a * x + b - Oy)² = (a * x + b - Oy) * (a * x + b - Oy)
+    //g = a * a * x * x + a * x * b - a * x * Oy + b * a * x + b * b - b * Oy - Oy * a * x - Oy * b + Oy * Oy;
+    //g = x * x * (a * a) + x * (a * b - a * Oy + b * a - Oy * a) + (b * b - b * Oy - Oy * b + Oy * Oy);
+    //g = x * x * (a * a) + x * 2 * a * (b - Oy) + (b - Oy) * (b - Oy);
+    //(2) dans (1) : x * x + Ox * Ox - 2 * x * Ox +    x * x * (a * a) + x * 2 * a * (b - Oy) + (b - Oy) * (b - Oy) = r * r
+    //r * r = x * x + x * (- 2 * Ox) + Ox * Ox + x * x * (a * a) + x * 2 * a * (b - Oy) + (b - Oy) * (b - Oy);
+    //r * r= x * x * (1 + a * a) + x * (-2 * Ox + 2 * a * (b - Oy)) + Ox * Ox + (b - Oy) * (b - Oy);
+    //x * x * (1 + a * a) + x * (-2 * Ox + 2 * a * (b - Oy)) + Ox * Ox + (b - Oy) * (b - Oy) - r * r= 0;
+    //Equation de la forme a * x²+ b * x + c = 0
+    public static Intersections2D _Intersections2D_DroiteCoupantUnCercle(Cercle c, Droite d)
+    {
+        if (d.a != float.MinValue && d.a != float.MaxValue)
+        {
+            float a = d.a;
+            float b = d.b;
+            float Ox = c.O.x;
+            float Oy = c.O.y;
+            float r = c.r;
+
+            float _a = (1 + a * a);
+            float _b = (-2 * Ox + 2 * a * (b - Oy));
+            float _c = Ox * Ox + (b - Oy) * (b - Oy) - r * r;
+            Vector2? X12 = _TrinomeDu2ndDegres(_a, _b, _c);
+            if (X12 == null)
+                return null;
+
+            Vector2 x12 = (Vector2)X12;
+            //trouve les Y avec l'équation de la droite
+            float Y1 = a * x12.x + b;
+            float Y2 = a * x12.y + b;
+
+            return new Intersections2D()
+            {
+                intersection_1 = new Vector2(x12.x, Y1),
+                intersection_2 = new Vector2(x12.y, Y2)
+            };
+        }
+        else
+        {
+            return new Intersections2D()
+            {
+                intersection_1 = new Vector2(0, c.r),
+                intersection_2 = new Vector2(0, -c.r)
+            };
+        }
+    }
+
+    public static Intersections3D _Intersections3D_DroiteCoupantUnCercle(Cercle c, Droite d)
+    {
+        if (d.a != float.MinValue && d.a != float.MaxValue)
+        {
+            float a = d.a;
+            float b = d.b;
+            float Ox = c.O.x;
+            float Oy = c.O.y;
+            float r = c.r;
+
+            float _a = (1 + a * a);
+            float _b = (-2 * Ox + 2 * a * (b - Oy));
+            float _c = Ox * Ox + (b - Oy) * (b - Oy) - r;
+            Vector2? X12 = _TrinomeDu2ndDegres(_a, _b, _c);
+            if (X12 == null)
+                return null;
+
+            Vector2 x12 = (Vector2)X12;
+            //trouve les Y avec l'équation de la droite
+            float Y1 = a * x12.x + b;
+            float Y2 = a * x12.y + b;
+
+            return new Intersections3D()
+            {
+                intersection_1 = new Vector3(x12.x, 0, Y1),
+                intersection_2 = new Vector3(x12.y, 0, Y2)
+            };
+        }
+        else
+        {
+            return new Intersections3D()
+            {
+                intersection_1 = new Vector3(0, 0, c.r),
+                intersection_2 = new Vector3(0, 0, -c.r)
+            };
+        }
+    }
+    
+    // a*x² + b*x + c = 0
+    public static Vector2? _TrinomeDu2ndDegres(float a, float b, float c)
+    {
+        // delta = b²-4*a*c
+        float d = b * b - 4 * a * c;
+        if (d < 0)
+            return null;
+        //x1 = (-b - d^0.5) / 2a
+        //x2 = (-b + d^0.5) / 2a
+        float x1 = (-b - Mathf.Pow(d, 0.5f)) / (2 * a);
+        float x2 = (-b + Mathf.Pow(d, 0.5f)) / (2 * a);
+        return new Vector2(x1, x2);
+    }
+
+    public static Vector2 _Intersection2D_DroiteCoupantUnCercle(Cercle c, Droite d, float angle)
+    {
+        Intersections2D i12 = _Intersections2D_DroiteCoupantUnCercle(c, d);
+
+        //i1 ou i2 ==> celui qui est le plus proche du point sans marge ??
+        float Bx = c.r * Mathf.Sin(angle / 180 * Mathf.PI);
+        float By = c.r * Mathf.Cos(angle / 180 * Mathf.PI);
+        Vector2 b = new Vector2(Bx, By);
+
+        if (Vector2.Distance(b, i12.intersection_1) < Vector2.Distance(b, i12.intersection_2))
+            return i12.intersection_1;
+        else
+            return i12.intersection_2;
+    }
+
+    public static Vector3 _Intersection3D_DroiteCoupantUnCercle(Cercle c, Droite d, float angle)
+    {
+        Intersections3D i12 = _Intersections3D_DroiteCoupantUnCercle(c, d);
+
+        //i1 ou i2 ==> celui qui est le plus proche du point sans marge ??
+        float Bx = c.r * Mathf.Sin(angle / 180 * Mathf.PI);
+        float By = c.r * Mathf.Cos(angle / 180 * Mathf.PI);
+        Vector3 b = new Vector3(Bx, 0, By);
+
+        if (Vector3.Distance(b, i12.intersection_1) < Vector3.Distance(b, i12.intersection_2))
+            return i12.intersection_1;
+        else
+            return i12.intersection_2;
+    }
+
+}
--- Assets/Resources/scripts/Sector3D.cs
+++ Assets/Resources/scripts/Sector3D.cs
@@ -27,57 +27,140 @@ public class Sector3D : MonoBehaviour
         return obj;
     }
 
+    static Vector3 FindPointOnCircleAndLine(float pente, float offset, float rayon, float angle)
+    {
+        // cercle centré : x² + y² = r² & droite coupant le cercle y = a*x + b
+        // en passant par le centre du cercle on sait qu'on aura toujours 2 intersections = 2 points = 2 solutions
+        // x² + (a²*x² + b² + 2*a*x*b) = r²      
+        // (1+a²)*x² + 2*a*b*x + b²-r² = 0
+        // si Ax²+Bx+C ==> delta = B²-4*A*C
+        float delta_Bx = Mathf.Pow(2 * pente * offset, 2) - 4 * (1 + pente * pente) * (offset * offset - rayon * rayon);
+        float Bx1 = (-(2 * pente * offset) - Mathf.Pow(delta_Bx, 0.5f)) / (2 * (1 + pente * pente));
+        float Bx2 = (-(2 * pente * offset) + Mathf.Pow(delta_Bx, 0.5f)) / (2 * (1 + pente * pente));
+        float By1, By2;
+
+        if (Bx1 == 0)
+        {
+            By1 = rayon;
+            By2 = -rayon;
+        }
+        else
+        {
+            By1 = pente * Bx1;
+            By2 = pente * Bx2;
+        }
+
+        Vector2 B1 = new Vector2(Bx1, By1);
+        Vector2 B2 = new Vector2(Bx2, By2);
+
+        //Bx1 ou Bx2 ==> celui qui est le plus proche du point sans marge ??
+        float Bx = rayon * Mathf.Sin(angle / 180 * Mathf.PI);
+        float By = rayon * Mathf.Cos(angle / 180 * Mathf.PI);
+        Vector3 b = new Vector3(Bx, 0, By);
+
+        if (Vector2.Distance(b, B1) < Vector2.Distance(b, B2))
+            b = B1;
+        else
+            b = B2;
+
+        return b;
+    }
+
     private static Mesh CreateMesh(float rayon_int, float rayon_ext, float angle_debut_deg, float angle_fin_deg, float marge, int nbrsegments)
     {
+        nbrsegments /= 2;
         if (nbrsegments < 1) nbrsegments = 1;
-        float E = 0.1f; //Epaisseur
-        float M = marge / nbrsegments;
+        //float E = 0.1f; //Epaisseur
 
-        float Ri = rayon_int;
-        float Re = rayon_ext - M / 4;
+        //        float M = marge / nbrsegments;
+        //        float Ri = rayon_int + M/2;
+        float M = marge;
+        float Ri = rayon_int + M;
+        float Re = rayon_ext;
         float a_0 = angle_debut_deg;
         float a_1 = angle_fin_deg;
+
+        float beta = a_1 - a_0;
+        float alpha = beta / 2;
+        float rotation = alpha + angle_debut_deg;
 
         List<Vector3> vertices = new List<Vector3>();
         List<Vector2> uv = new List<Vector2>();
         List<int> triangles = new List<int>();
 
-        float Ox = (M) * Mathf.Sin((a_0 + a_1) / 2 / 180 * Mathf.PI);
-        float Oy = (M) * Mathf.Cos((a_0 + a_1) / 2 / 180 * Mathf.PI);
+        // premiers points en partant du milieu (sur l'axe Y)
+        Vector2 A = new Vector2(0, Ri);
+        Vector2 B = new Vector2(0, Re);
+        vertices.Add(A);
+        vertices.Add(B);
+        uv.Add(Vector3.forward);
+        uv.Add(Vector3.forward);
 
-        float Ax = Ox + (Ri) * Mathf.Sin(a_0 / 180 * Mathf.PI);
-        float Ay = Oy + (Ri) * Mathf.Cos(a_0 / 180 * Mathf.PI);
-        float Bx = Ox + (Re) * Mathf.Sin(a_0 / 180 * Mathf.PI);
-        float By = Oy + (Re) * Mathf.Cos(a_0 / 180 * Mathf.PI);
-        Vector3 a = new Vector3(Ax, 0, Ay);
-        Vector3 b = new Vector3(Bx, 0, By);
-        vertices.Add(a);
-        vertices.Add(b);
-        uv.Add(Vector3.forward);
-        uv.Add(Vector3.forward);
+        Vector2 O = new Vector2(0, 0);
+
+        //origine prime (à cause de la marge)
+        float Opy = M / Mathf.Sin((90 - alpha) / 180 * Mathf.PI);
+        Vector2 Op = new Vector2(0, Opy);
+        float a1 = Mathf.Tan((90 - alpha) / 180 * Mathf.PI);
+
+        // derniers points côté fin rotation horaire
+        Math_JJ.Droite d = new Math_JJ.Droite() { a = a1, b = Opy };
+        Math_JJ.Cercle ce = new Math_JJ.Cercle() { O = O, r = Re };
+        Math_JJ.Cercle ci = new Math_JJ.Cercle() { O = O, r = Ri };
+        Vector2 Y = Math_JJ._Intersection2D_DroiteCoupantUnCercle(ci, d, alpha);
+        Vector2 Z = Math_JJ._Intersection2D_DroiteCoupantUnCercle(ce, d, alpha);
 
         int it = 0; //indextriangles
         for (int i = 1; i < nbrsegments + 1; i++)
         {
-            float a_01 = a_0 + (a_1 - a_0) * i / nbrsegments;
+            float a_01 = alpha * i / nbrsegments;
+            a1 = Mathf.Tan((90 - a_01) / 180 * Mathf.PI);
+            d = new Math_JJ.Droite() { a = a1, b = Opy };
+            A = Math_JJ._Intersection2D_DroiteCoupantUnCercle(ci, d, a_01);
+            B = Math_JJ._Intersection2D_DroiteCoupantUnCercle(ce, d, a_01);
 
-            Ax = Ox + (Ri) * Mathf.Sin(a_01 / 180 * Mathf.PI);
-            Ay = Oy + (Ri) * Mathf.Cos(a_01 / 180 * Mathf.PI);
-            Bx = Ox + (Re) * Mathf.Sin(a_01 / 180 * Mathf.PI);
-            By = Oy + (Re) * Mathf.Cos(a_01 / 180 * Mathf.PI);
-
-            a = new Vector3(Ax, 0, Ay);
-            b = new Vector3(Bx, 0, By);
-            vertices.Add(a);
-            vertices.Add(b);
+            vertices.Add(A);
+            vertices.Add(B);
 
             uv.Add(Vector3.forward);
             uv.Add(Vector3.forward);
 
             triangles.AddRange(new int[] { it, it+1, it+2, //a, b, c
-                                          it+1, it+3, it+2, //d, c, b
-                                        });
+                                              it+1, it+3, it+2, //b, d, c
+                                            });
             it += 2;
+        }
+
+        A = new Vector2(0, Ri);
+        B = new Vector2(0, Re);
+        vertices.Add(A);
+        vertices.Add(B);
+        uv.Add(Vector3.forward);
+        uv.Add(Vector3.forward);
+        it += 2;
+        for (int i = 1; i < nbrsegments + 1; i++)
+        {
+            float a_01 = alpha * i / nbrsegments;
+            a1 = Mathf.Tan((90 - a_01) / 180 * Mathf.PI);
+            d = new Math_JJ.Droite() { a = a1, b = Opy };
+            A = Math_JJ._Intersection2D_DroiteCoupantUnCercle(ci, d, a_01);
+            B = Math_JJ._Intersection2D_DroiteCoupantUnCercle(ce, d, a_01);
+
+            vertices.Add(new Vector3(-A.x, A.y));
+            vertices.Add(new Vector3(-B.x, B.y));
+            uv.Add(Vector3.forward);
+            uv.Add(Vector3.forward);
+
+            triangles.AddRange(new int[] { it, it+2, it+1, //a, c, b
+                                              it+1, it+2, it+3, //b, c, d
+                                            });
+            it += 2;
+        }
+
+        //rotation de tous les points (= du secteur) à la position demandée
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            vertices[i] = Quaternion.Euler(0, 0, rotation) * vertices[i];
         }
 
         var mesh = new Mesh();
